8370c20c8702817a7e8e52f4ad4d2a97697d6f9f
AbdulRafey19 <abdulrafeybeig2002@gmail.com>
2024-04-24 21:22:27 +0500

    first commit

---
+++ MP project.py
@@ -0,0 +1,89 @@
+import speech_recognition as sr
+from pyfirmata import Arduino, SERVO, util
+from time import sleep
+
+r=sr.Recognizer()
+mic = sr.Microphone(device_index=1)
+
+port = 'COM4'
+pin1 = 13
+pin2 = 12
+pin3 = 11
+pin4 = 10
+pin5 = 9
+
+board = Arduino(port)
+
+board.digital[pin1].mode=SERVO
+board.digital[pin2].mode=SERVO
+board.digital[pin3].mode=SERVO
+board.digital[pin4].mode=SERVO
+board.digital[pin5].mode=SERVO
+
+def rotate(pin,angle):
+    board.digital[pin].write(angle)
+    sleep(0.01)
+    
+with mic as source:
+    r.adjust_for_ambient_noise(source)
+    while True:
+        audio = r.listen(source)
+        try:
+            if r.recognize_google(audio) =='first finger':
+                print("switch is on")
+                for i in range(0,360):
+                    rotate(pin1,i)
+                # for i in range(360,0):
+                #     rotate(pin1,i)
+            
+            elif r.recognize_google(audio) =='middle finger':
+                print("switch is on")
+                for i in range(0,360):
+                    rotate(pin2,i)
+#                 for i in range(360,0):
+#                     rotate(pin2,i)
+
+            elif r.recognize_google(audio) =='ring finger':
+                print("switch is on")
+                for i in range(0,360):
+                    rotate(pin3,i)
+#                 for i in range(360,0):
+#                     rotate(pin3,i)
+
+            elif r.recognize_google(audio) =='small finger':
+                print("switch is on")
+                for i in range(0,360):
+                    rotate(pin4,i)
+#                 for i in range(360,0):
+#                     rotate(pin4,i)
+
+            # elif r.recognize_google(audio) =='pollex':
+            #     print("switch is on")
+            #     for i in range(0,360):
+            #         rotate(pin5,i)
+#                 for i in range(360,0):
+#                     rotate(pin5,i)
+            # elif r.recognize_google(audio) =='thum':
+            #     print("switch is on")
+            #     for i in range(0,360):
+            #         rotate(pin5,i)
+                    
+            # elif r.recognize_google(audio) =='ham':
+            #     print("switch is on")
+            #     for i in range(0,360):
+            #         rotate(pin5,i)
+                    
+            # elif r.recognize_google(audio) =='hum':
+            #     print("switch is on")
+            #     for i in range(0,360):
+            #         rotate(pin5,i)
+            
+            elif r.recognize_google(audio) =='fat finger':
+                print("switch is on")
+                for i in range(0,360):
+                    rotate(pin5,i)
+            else:
+                print("something")
+        
+        except:
+            print("no audio")
